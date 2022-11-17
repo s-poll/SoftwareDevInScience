7ab3312425ac70cf01c7f3b5c48ed9c0335413e8
Stefan Poll <s.poll@fz-juelich.de>
2022-11-17 12:27:54 +0100

    include square function

--- printNumbers/printNumbers.py
+++ printNumbers/printNumbers.py
@@ -27,24 +27,27 @@
 """
 Usage:
   printNumbers.py -h --help
-  printNumbers.py [--fibonacci|--factorial] <operand>
+  printNumbers.py [--fibonacci|--factorial|--square] <operand>
 
 Options:
   -h --help       Print usage.
   --fibonacci     Print the fibonacci sequence.
   --factorial     Print the factorial.
+  --square        Print the square.
 """
 
 from docopt import docopt
 from parameters import *
 from functions.fibonacci import *
 from functions.factorial import *
+from functions.square import *
 
 #
 # FUNCTION TABLE
 #
 functionTable = { CONST_FUNC_CODE_FIBONACCI : FibonacciSequence,
                   CONST_FUNC_CODE_FACTORIAL : Factorial,
+                  CONST_FUNC_CODE_SQUARE    : Square,
                 }
 
 #
@@ -67,3 +70,5 @@ if __name__ == '__main__':
         print('fib(' + str(params.operand) + ') =', result)
     elif params.functionIndex == CONST_FUNC_CODE_FACTORIAL:
         print(str(params.operand) + '! =', str(result))
+    elif params.functionIndex == CONST_FUNC_CODE_SQUARE:
+        print(str(params.operand) + ' squared is ', str(result))    
